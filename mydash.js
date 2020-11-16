ef3636ebbb863f452fb8f60eb17895eb9e89a910
Varvara Moiseenko <95varya@mail.ru>
2020-11-16 10:38:37 +0500

    feat: add first() and last() array helpers

--- mydash.js
+++ mydash.js
@@ -3,3 +3,10 @@ function last(list) {
             ? list[list.length - 1] 
             : undefined;
 }
+
+function first(list) {
+	  return (Array.isArray(list) && list[0]) 
+	    ? list[0] 
+	  : undefined;
+}
+
