4ec964384a8d9f095b71267b73e7a8b4a86e0dec
Varvara Moiseenko <95varya@mail.ru>
2020-11-16 16:31:23 +0500

    feat: Array Range

--- mydash.js
+++ mydash.js
@@ -1,12 +1,46 @@
+// Array: Last Element
 function last(list) {
 	return (Array.isArray(list) && list[list.length - 1]) 
             ? list[list.length - 1] 
             : undefined;
 }
 
+// Array: First Element
 function first(list) {
 	  return (Array.isArray(list) && list[0]) 
 	    ? list[0] 
 	  : undefined;
 }
 
+// Array: Range
+function range(start, end, step = 1) {
+    if (arguments.length === 1) {
+        end = start;
+        start = 0;
+    }
+
+    step = Math.abs(step)
+  
+    let result = [];
+    if (end > 0) {
+      for (let i = start; i < end; i += step) {
+        if (step === 0) {
+          result.push(start);
+          i++;
+        } else {
+          result.push(i);
+        }
+      }
+    } else {
+      for (let i = start; i > end; i -= step) {
+        if (step === 0) {
+          result.push(start);
+          i--;
+        } else {
+          result.push(i);
+        }
+      }
+    }
+
+    return result;
+}
