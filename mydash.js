654863c02945c516ade7578f7a2eee1cd11ec7bd
Varvara Moiseenko <95varya@mail.ru>
2020-11-17 11:43:05 +0500

    Update mydash.js

--- mydash.js
+++ mydash.js
@@ -13,34 +13,64 @@ function first(list) {
 }
 
 // Array: Range
-function range(start, end, step = 1) {
-    if (arguments.length === 1) {
-        end = start;
-        start = 0;
-    }
+// function range(start, end, step = 1) {
+//     if (arguments.length === 1) {
+//         end = start;
+//         start = 0;
+//     }
 
-    step = Math.abs(step)
+//     step = Math.abs(step)
   
-    let result = [];
-    if (end > 0) {
-      for (let i = start; i < end; i += step) {
-        if (step === 0) {
-          result.push(start);
-          i++;
-        } else {
-          result.push(i);
-        }
-      }
-    } else {
-      for (let i = start; i > end; i -= step) {
-        if (step === 0) {
-          result.push(start);
-          i--;
-        } else {
-          result.push(i);
-        }
-      }
-    }
+//     let result = [];
+//     if (end > 0) {
+//       for (let i = start; i < end; i += step) {
+//         if (step === 0) {
+//           result.push(start);
+//           i++;
+//         } else {
+//           result.push(i);
+//         }
+//       }
+//     } else {
+//       for (let i = start; i > end; i -= step) {
+//         if (step === 0) {
+//           result.push(start);
+//           i--;
+//         } else {
+//           result.push(i);
+//         }
+//       }
+//     }
 
-    return result;
+//     return result;
+// }
+
+function rangeRight(start, end, step) {
+	return range(start, end, step, true);
+}
+
+function range(start, end, step, isRight) {  
+    // Ensure the sign of `-0` is preserved.
+    start = start
+    if (end === undefined) {
+      end = start
+      start = 0
+    } else {
+      end = end
+    }
+    step = step === undefined ? (start < end ? 1 : -1) : step
+    return baseRange(start, end, step, isRight)
+  
+}
+
+function baseRange(start, end, step, fromRight) {
+  let index = -1
+  let length = Math.max(Math.ceil((end - start) / (step || 1)), 0)
+  const result = new Array(length)
+
+  while (length--) {
+    result[fromRight ? length : ++index] = start
+    start += step
+  }
+  return result
 }
